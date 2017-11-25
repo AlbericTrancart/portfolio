5f1ac5d760df2199cce571660305c7023aa25a50
Albéric Trancart <alberic@trancart.net>
2017-11-25 11:09:23 +0100

    Add proper config for prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,30 +1,40 @@
 const path = require('path');
 const webpack = require('webpack');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const context = `${__dirname}/app`;
 module.exports = {
   devtool: 'source-map',
-  context,
   entry: [
-    path.join(__dirname, './app/index.js'),
+    path.join(__dirname, './app/index.jsx'),
   ],
   resolve: {
     alias: {
       app: 'app',
     },
     modules: [
-      context,
+      path.join(__dirname, '/app'),
       'node_modules',
     ],
     extensions: ['.js', '.jsx'],
   },
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/public/',
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name]-[hash].js',
+    publicPath: '/',
   },
   plugins: [
-    // Handles uglifying js
+    new FaviconsWebpackPlugin({
+      logo: path.join(__dirname, '/app/img/profile.jpg'),
+      inject: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: 'app/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html',
+    }),
+    new ExtractTextPlugin('[name]-[hash].min.css'),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
         warnings: false,
@@ -39,7 +49,7 @@ module.exports = {
   module: {
     rules: [{
       enforce: 'pre',
-      test: /\.js$/,
+      test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'eslint-loader',
       options: {
@@ -48,12 +58,18 @@ module.exports = {
         failOnError: false,
       },
     }, {
-      test: /\.js?$/,
+      test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
     }, {
+      test: /\.(png|jpe?g|gif)$/,
+      loader: 'url-loader?limit=8192', // inline base64 URLs for <=8k images, direct URLs for the rest
+    }, {
       test: /\.styl$/,
-      loader: 'style-loader!css-loader!stylus-loader',
+      loader: ExtractTextPlugin.extract({
+        fallback: { loader: 'style-loader' },
+        use: ['css-loader', { loader: 'stylus-loader' }],
+      }),
     }],
   },
 };
