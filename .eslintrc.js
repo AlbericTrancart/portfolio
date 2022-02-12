b99fadeb35a01c91faa817e12f185b1eadd5234b
Albéric Trancart <alberic@trancart.net>
2022-02-12 17:04:35 +0100

    Run Clinter to update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,63 +1,125 @@
-const path = require("path");
-
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended',"airbnb", "prettier", "prettier"],
-  plugins: ["react", "prettier", "jsx-a11y",'@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'prettier',
+    'eslint:recommended',
+    'react-app',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'prettier', '@typescript-eslint', 'import', 'jsx-a11y'],
   env: {
     browser: true,
     es6: true,
     node: true,
   },
   settings: {
-    "import/resolver": {
-      webpack: {
-        config: {
-          resolve: {
-            modules: [path.resolve(__dirname, "src"), "node_modules"],
-          },
-        },
-      },
+    react: {
+      version: 'detect',
     },
   },
   rules: {
-    "react/no-unescaped-entities": 0,
-    "no-var": "error", // optional, recommended when using es6+
-    "no-unused-vars": 1, // recommended
-    "arrow-spacing": ["error", { before: true, after: true }], // recommended
-    indent: 0,
-    "comma-dangle": [
-      "error",
+    'react/no-unescaped-entities': 0,
+    'no-var': 'error',
+    'no-unused-vars': 1,
+    'arrow-spacing': [
+      'error',
       {
-        objects: "only-multiline",
-        arrays: "only-multiline",
-        imports: "never",
-        exports: "never",
-        functions: "never",
+        before: true,
+        after: true,
       },
     ],
-
-    // react plugin - options
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
-    "react/jsx-filename-extension": [1, { extensions: [".js"] }],
-    "react/jsx-props-no-spreading": 0,
-    "react/prop-types": 0,
-    "no-plusplus": [
-      "error",
+    indent: 0,
+    'comma-dangle': [
+      'error',
+      {
+        objects: 'only-multiline',
+        arrays: 'only-multiline',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never',
+      },
+    ],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js'],
+      },
+    ],
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0,
+    'no-plusplus': [
+      'error',
       {
         allowForLoopAfterthoughts: true,
       },
     ],
-    "no-loop-func": 0,
-    "react/no-array-index-key": 0,
+    'no-loop-func': 0,
+    'react/no-array-index-key': 0,
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/explicit-member-accessibility': 0,
     '@typescript-eslint/camelcase': 0,
     '@typescript-eslint/interface-name-prefix': 0,
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/no-non-null-assertion': 0,
     '@typescript-eslint/explicit-module-boundary-types': 0,
+    curly: ['error', 'all'],
+    eqeqeq: ['error', 'smart'],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+    'no-shadow': [
+      'error',
+      {
+        hoist: 'all',
+      },
+    ],
+    'prefer-const': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['external', 'builtin'],
+          'internal',
+          ['parent', 'sibling', 'index'],
+        ],
+      },
+    ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
+    ],
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      },
+    ],
+    'react/no-string-refs': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
+  root: true,
 };
